c0ce9768008e3f9ee503cb2e0faa84a4a4b3235a
lllhy9428 <90584270+lllhy9428@users.noreply.github.com>
2021-12-28 09:59:42 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ plugin_url = "https://github.com/crealitycloud/OctoPrint-Crealitycloud"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = ["OctoPrint>1.3.8", "aliyun-iot-linkkit==1.2.3"]
+plugin_requires = ["OctoPrint>1.3.8", "aliyun-iot-linkkit==1.2.3", "paho_mqtt==1.5.1"]
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
