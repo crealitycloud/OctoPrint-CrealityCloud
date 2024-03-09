13c46207636b54a554040a53acf4e3fbf16200f7
jneilliii <jneilliii+github@gmail.com>
2024-03-09 11:25:47 -0500

    add other missing dependencies
    noticed other people on the OctoPrint forum requiring to add these additional packages

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ plugin_url = "https://github.com/crealitycloud/OctoPrint-Crealitycloud"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = ["OctoPrint>1.7.0", "paho_mqtt<2", "pyjwt", "tb-mqtt-client==1.2"]
+plugin_requires = ["OctoPrint>1.7.0", "paho_mqtt<2", "pyjwt", "tb-mqtt-client==1.2", "aliyun-iot-linkkit", "aiortc"]
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
